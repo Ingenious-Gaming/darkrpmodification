7e539c45b72b4d74738e231e914dca35a6443e19
Brandon Davidson <brandondcpp@gmail.com>
2015-02-05 19:38:32 -0600

    Test AK-47 shipment.

--- lua/darkrp_customthings/shipments.lua
+++ lua/darkrp_customthings/shipments.lua
@@ -17,3 +17,23 @@ http://wiki.darkrp.com/index.php/DarkRP:CustomShipmentFields
 
 Add shipments and guns under the following line:
 ---------------------------------------------------------------------------]]
+
+DarkRP.createShipment(
+    "AK-47 Shipment",
+    {
+        model = "models/weapons/w_ak47_m9k.mdl", -- The model of the item that hovers above the shipment
+        entity = "m9k_ak47", -- the entity that comes out of the shipment
+        price = 100, -- the price of one shipment
+        amount = 10, -- how many of the item go in one purchased shipment
+        separate = true, -- whether the item is sold separately (usually used for guns)
+        pricesep = 100, -- the price of a separately sold item
+        noship = false, -- whether this item has a shipment
+        allowed = {TEAM_GUN, TEAM_MEDIC}, -- OPTIONAL, which teams are allowed to buy this shipment/separate gun
+        shipmodel = "models/items/item_item_crate.mdl", -- OPTIONAL, the model of the shipment (this crate is the default)
+        spareammo = 10, -- OPTIONAL, Add ammo to the default amount of ammo for every weapon
+        clip1 = 30, -- OPTIONAL, The amount of bullets in the primary clip by default
+        clip2 = 30, -- OPTIONAL, The amount of bullets in the secondary clip by default
+        buttonColor = Color(255, 255, 255, 255), -- Optional: The color of the button in the F4 menu,
+        label = "AK-47", -- Optional: the text on the button in the F4 menu
+    }
+)
